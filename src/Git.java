546f760e347ed8a20ee7734ca5567f42fa4bbc7c
tttsurovikin <stimofey03@gmail.com>
2023-10-30 12:29:54 +0100

    first commit Second commit

--- src/Git.java
+++ src/Git.java
@@ -1,6 +1,7 @@
 public class Git {
     public static void main(String []args){
         System.out.println("Hello world and Git I am Tim ");
+        System.out.println("Secod commit for Git ");
 
     }
 
